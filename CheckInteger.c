56cbad0757bf1d6a9216b90cf43bdc55de638682
16naman11 <141851295+16naman11@users.noreply.github.com>
2024-05-01 00:44:43 +0530

    checking if integer is +ve, -ve or 0
    Simple C program to determine whether an integer is positive, negative, or zero. This concise code efficiently evaluates the input number and provides a clear indication of its sign, aiding in basic numerical analysis and decision-making.

---
+++ CheckInteger.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+   
+   int a;
+   printf ("\n\nenter an integer: ");
+   scanf ("%d", &a);
+   
+   if(a == 0){
+       printf("\nthe integer is 0");
+      
+   }
+   else if (a>0){
+       printf("\n the integer is positive");
+   }
+   
+   else{
+       printf("\nthe integer is negative");
+       
+   }
+   
+   int b;
+   printf ("\n\nenter an integer: ");
+   scanf ("%d", &b);
+   
+   if(b == 0){
+       printf("\nthe integer is 0");
+      
+   }
+   else if (b>0){
+       printf("\n the integer is positive");
+   }
+   
+   else{
+       printf("\n\nthe integer is negative");
+       
+   }
+   
+   
+   int c;
+   printf ("\n\nenter an integer: ");
+   scanf ("%d", &c);
+   
+   if(c == 0){
+       printf("\nthe integer is 0");
+      
+   }
+   else if (c>0){
+       printf("\n the integer is positive");
+   }
+   
+   else{
+       printf("\nthe integer is negative");
+       
+   }
+   
+    return 0;
+}
+
