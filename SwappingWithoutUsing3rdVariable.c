fac5a986758f11df8e2a5490d0214a1ad24e51e9
16naman11 <141851295+16naman11@users.noreply.github.com>
2024-04-30 20:47:24 +0530

    Swapping without 3rd variable
    Simple C program showcasing the swapping of two integers without employing a third variable. This code utilizes arithmetic operations to interchange values between variables, demonstrating a straightforward method for value exchange.

---
+++ SwappingWithoutUsing3rdVariable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() 
+{
+    int a,b;
+    printf("enter 2 integers: ");
+    scanf("%d%d", &a , &b);
+    printf ("before swapping a=%d , b=%d", a,b);
+    
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+    printf("after swapping a=%d , b =%d", a,b);
+    return 0;
+}
