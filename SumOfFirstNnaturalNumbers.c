f274caec40257f5dda33ab6d24b8b56e13fc0228
16naman11 <141851295+16naman11@users.noreply.github.com>
2024-05-05 18:16:39 +0530

    Sum of First N natural numbers.
    This code calculates the sum of the firds N natural numbers.

---
+++ SumOfFirstNnaturalNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n,sum;
+    printf ("enter the value of n: \n");
+    scanf("%d",&n);
+    sum = (n + n*n)/2;
+    printf(" value of sum is : %d \n" , sum);
+
+    return 0;
+}
