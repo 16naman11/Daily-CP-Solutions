d42ea89aeca1048038503a713905f16614f6cf58
Naman Shah <141851295+16naman11@users.noreply.github.com>
2024-05-30 16:29:35 +0530

    Check Digit or Not

---
+++ CheckDigitorNot.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+    printf("enter value of a : ");
+    scanf("%c", &a);
+    if(a>=48&&a<=57){
+        printf("the key pressed is a digit ");
+        
+    }
+    else
+    printf("the key pressed is not a digit");
+    return 0;
+    
+   
+}
