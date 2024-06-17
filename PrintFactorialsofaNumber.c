0afffb9cf4a978cdc84238572659e96de2a6fa59
Naman Shah <141851295+16naman11@users.noreply.github.com>
2024-06-17 10:17:33 +0530

    Print factorials of a number

---
+++ PrintFactorialsofaNumber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    printf("enter a number : \n");
+    scanf("%d",&n);
+    for(i=1;i<n;i++){
+        if(n%i==0){
+            printf("%d is a factorial of %d\n",i,n);
+        }
+    }
+    return 0;
+}
