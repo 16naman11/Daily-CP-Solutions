a68ed2f9370d05c631baa31a66cf5503953f76db
Naman Shah <141851295+16naman11@users.noreply.github.com>
2024-09-01 12:25:32 +0530

    Default constructor

---
+++ DefaultConstructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class MyClass {
+public:
+    // Default constructor
+    MyClass() {
+        // Add your code here
+        std::cout << "Default constructor called!" << std::endl;
+    }
+};
+
+int main() {
+    MyClass obj; // Creating an object of MyClass
+    return 0;
+}
